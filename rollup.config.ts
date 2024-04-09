03fc5e2374083cca04b35e59017a128272e6f812
Primal Zed <primal.zed@gmail.com>
2024-04-09 16:15:19 -0500

    typescript rollup.config.ts

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { type RollupOptions } from 'rollup';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
@@ -12,6 +13,6 @@ export default {
 		commonjs(),
 		nodeResolve(),
 		json(),
-		typescript({ tsconfig: 'tsconfig.node.json' })
+		typescript({ tsconfig: 'tsconfig.node.json' }),
 	],
-};
+} satisfies RollupOptions;
